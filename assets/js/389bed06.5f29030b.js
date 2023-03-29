4b4c3d6c2a6e8dce511f3dfd2193f75f95bcf36c
CIownxz <fatestheadgamin@gmail.com>
2023-03-29 00:09:51 -0400

    Built and published by Moonwave

---
+++ assets/js/389bed06.5f29030b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[862],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/Clownxz/Future/blob/master","baseUrl":"/Future/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/b4fd4062.d2aabf5c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[338],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Future","label":"\u200bFuture"}]')}}]);
