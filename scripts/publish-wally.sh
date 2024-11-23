0b6f0605f76ccae639c7e748579e8cb5783f3d24
YetAnotherClown <yetanotherclown@proton.me>
2024-11-23 03:17:16 -0500

    Setup publish-wally

---
+++ scripts/publish-wally.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+rojo sourcemap default.project.json -o sourcemap.json
+darklua process --config .darklua.json src/ dist/src
+
+cp README.md dist/README.md
+cp LICENSE dist/LICENSE
+cp wally.toml dist/wally.toml
+cp wally.lock dist/wally.lock
+
+cp build.project.json dist/default.project.json
+sed -i 's/dist\/src/src/' dist/default.project.json
+
+cd ./dist
+
+if [ "$1" = "--publish" ]; then
+    wally publish
+else
+    wally package --output release.zip
+fi
